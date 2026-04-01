426643675f0c832ea80a1762378e09f10fb65c65
Ayedimohamed <248538731+mohamadayedi23-creator@users.noreply.github.com>
2026-04-01 21:00:25 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { IonApp, IonContent, IonSpinner } from '@ionic/angular/standalone';
+import { IonApp } from '@ionic/angular/standalone';
 import { NavbarComponent } from './core/layout/navbar/navbar.component';
 import { FooterComponent } from './core/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IonApp, IonContent, IonSpinner, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, IonApp, NavbarComponent, FooterComponent],
   template: `
     <ion-app>
       <app-navbar></app-navbar>
-      <ion-content [fullscreen]="true">
+      <main>
         <router-outlet></router-outlet>
-      </ion-content>
+      </main>
       <app-footer></app-footer>
     </ion-app>
   `
